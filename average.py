79669b14ed884267b5ba574cfea806f49b0d8185
lu4ckex <lu4ckex@outlook.com>
2024-08-13 11:53:45 -0300

    feat: initial implementation of the average calculator\n\nIncludes input and calculation for theoretical, practical, and assignment grades.

---
+++ average.py
@@ -0,0 +1,10 @@
+print ('--------- Student Average Calculator v1.0 ---------')
+print ()
+
+notat = float(input('Enter the theoretical grade: '))
+notap = float(input('Enter the practical grade: '))
+notatr = float(input('Enter the assignment grade: '))
+               
+media = (notat + notap + notatr) / 3
+print()
+print ('The students average is: ', media)
